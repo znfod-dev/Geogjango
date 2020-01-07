5a74349e7b9bc039d4a5cc6abefb0ceef4f01f29
JongHyun Park <znfod@JongHyuns-MacBook-Pro.local>
2020-01-07 19:52:50 +0900

    change models

--- app/geo/models.py
+++ app/geo/models.py
@@ -6,10 +6,19 @@ from django.contrib.gis.geos import Point
 # Create your models here.
 class Location(models.Model):
     name = models.CharField(max_length=200)
-    # brew install gdal --HEAD
-    # sudo easy_install GDAL
+    lng = models.DecimalField(decimal_places=8, max_digits=12, null=True, blank=True)
+    lat = models.DecimalField(decimal_places=8, max_digits=12, null=True, blank=True)
+    latlng = models.PointField(null=True, blank=True)
+
+    def __init__(self):
+        print('init')
+        self.latlng = Point(x=float(self.lat), y=float(self.lng))
+
+        super(Location, self).__init__()
 
     def __str__(self):
         return self.name
 
+    def __str__(self):
+        return f'name[{self.name}] : {self.lat}, {self.lng}, {self.latlng}'
 
